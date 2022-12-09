96a1fbab8269518575097a233f88b636612d2bed
Joseph Akpan <akpannath@yahoo.com>
2022-12-08 17:40:58 -0800

    Task 7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,6 @@ int main(void)
 	printf("Size of an int: %1u byte(s)\n", sizeof(int));
 	printf("Size of a long int: %1u byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %1u byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %1u byte(s)\n", sizeof (float));
-	return (0);
-
+	printf("Size of a float: %1u byte(s)\n", sizeof(float));
+	return (0;
 }
