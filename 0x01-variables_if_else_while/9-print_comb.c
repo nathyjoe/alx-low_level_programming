12b04864608c6548341507112db7093b8a892ca5
Joseph Akpan <akpannath@yahoo.com>
2022-12-11 12:13:40 -0800

    Updated task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,13 @@ int main(void)
 	int n;
 
 	for (n = 48; n < 58; n++)
-		putchar (n);
+	{	putchar (n);
 		if (n != 57)
 		{
 			putchar (',');
 			putchar (' ');
 		}
+	}
 	putchar ('\n');
 	return (0);
 }
