ac0dc44f5b5858bbb3fc88df91b75205e51db85c
Joseph Akpan <akpannath@yahoo.com>
2022-12-14 21:13:39 -0800

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet lower case
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+_putchar('\n');
+
+}
