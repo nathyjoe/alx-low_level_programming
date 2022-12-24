fc4512e432a6918e5a0163a50dae97c3fedf7e11
Joseph Akpan <akpannath@yahoo.com>
2022-12-23 21:14:29 -0800

    Task 4

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+root@b19751a1d3dc:~/alx-low_level_programming#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1
+ * On error, -1 is returned and is set appropriatly
+ */
+int _putchar(char c)
+{
+	return (write (1, &c, 1));
+}
+
