d8018163c5a8d558733400698de803574c3286bd
Joseph Akpan <akpannath@yahoo.com>
2023-01-02 14:21:05 -0800

    Task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_strings - set the value of a point to a char
+ * @s: source adress.
+ * @to: target adress.
+ * Return: no return.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
