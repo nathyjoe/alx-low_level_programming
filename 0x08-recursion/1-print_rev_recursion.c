425508903006235a6f9f083d6ba3994505a8e807
Joseph Akpan <akpannath@yahoo.com>
2023-01-04 09:13:42 -0800

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+/*
+ * file: 0-puts_recursion.c
+ * Auth: sum
+ */
+
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: The string to be printed.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_print_rev_recursion(s + 1);
+	}
+
+}
